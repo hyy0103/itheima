f9687098a58bc0916d1c0658ec01b21ed925c915
hyy0103 <huang021201@gmail.com>
2022-08-19 20:37:18 +0300

    P200

---
+++ src/Array/ArrayDemo.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Arrays;
+
+/*
+    Array类包含用于操作数组的各种方法
+
+    public static String toString(int[] a)：返回指定数组的内容的字符串表达形式
+    public static void sort(int[] a)：按照数字顺序排列指定的数组
+ */
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        //定义一个数组
+        int[] arr = {24, 69, 80, 57, 13};
+        System.out.println("排序前：" + Arrays.toString(arr));
+
+        Arrays.sort(arr);
+        System.out.println("排序后：" + Arrays.toString(arr));
+
+    }
+}
